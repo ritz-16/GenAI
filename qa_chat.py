4c18e0395e4fa9418d448585566a7ccb049455ce
ritz-16 <myotherface16@gmail.com>
2024-07-09 04:02:11 +0530

    qa_chatbot

--- qa_chat.py
+++ qa_chat.py
@@ -17,6 +17,30 @@ def get_gemeni_response(question):
     response = chat.send_message(question,stream=True)
     return response
 
-st.set_page_config
+st.set_page_config(page_title="Gemini QA Chatbot")
+
+st.header("Gemini Application")
+
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
+
+input=st.text_input("Input: ",key="input")
+submit=st.button("Ask your query")
+
+if submit and input:
+    response=get_gemeni_response(input)
+    st.session_state['chat_history'].append(("You",input))
+    st.subheader("Response:")
+    for chunk in response: #Because we wrote stream=True
+        st.write(chunk.text)
+        st.session_state['chat_history'].append(("Bot",chunk.text))
+
+    st.subheader("The Chat history is:")
+
+for role,message in st.session_state['chat_history']:
+    
+    st.write(f"{role}: {message}")
+
+
 
 
